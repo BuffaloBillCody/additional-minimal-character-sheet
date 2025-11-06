1c6bf4dbffc84ea45d38a1f936b14f6bbbae34dc
BuffaloBillCody <benjaminleebrown@gmail.com>
2025-11-05 19:50:31 -0700

    Make entrypoint check for just data folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 # Path inside container where app lives
-APP_DIR="/var/www/html"
+APP_DIR="/var/www/html/data"
 cd $APP_DIR
 
 # Check if APP_DIR is empty (e.g., first container run)
 if [ -z "$(ls -A $APP_DIR)" ]; then
+    mkdir -p /var/www/html/data
     echo "creating .env file..."
     touch .env
     echo "BREVO_API_KEY=" > .env
@@ -16,7 +17,6 @@ if [ -z "$(ls -A $APP_DIR)" ]; then
 fi
 
 echo "Changing Database File Permissions..."
-mkdir -p /var/www/html/data
 chown -R www-data:www-data /var/www/html/data
 chmod -R 775 /var/www/html/data
 echo "Docker entrypoint script finished"
